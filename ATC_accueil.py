9f5dc03ebf108fb58ae7eda8dd54126e40503379
ashyl <k299achille@gmai.com>
2025-11-17 16:42:41 +0100

    rectification des proportions des boutons reims et brest

--- ATC_accueil.py
+++ ATC_accueil.py
@@ -225,9 +225,9 @@ class Ui_ATC_accueil(object):
 "}")
 
         # ---- POSITIONNEMENT ----
-        self.overlay.addWidget(self.ATC_brestlfrr, 1, 0, 1, 1)
+        self.overlay.addWidget(self.ATC_brestlfrr, 0, 0, 2, 1)
         self.overlay.addWidget(self.ATC_parislfff, 0, 1, 2, 2)
-        self.overlay.addWidget(self.ATC_reimslfee, 1, 3, 1, 1)
+        self.overlay.addWidget(self.ATC_reimslfee, 0, 3, 2, 1)
         self.overlay.addWidget(self.ATC_bordeauxlfbb, 2, 0, 2, 2)
         self.overlay.addWidget(self.ATC_marseillelfmm, 2, 2, 2 ,2)
         #on positionne les boutons dans le tableau
