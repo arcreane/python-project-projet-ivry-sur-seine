ecc000605bb049f461b329aa883f18ef1e3acc4d
nocalle <noemaglott@gmail.com>
2025-11-29 19:23:30 +0100

    restructuration du programme en 2 fonction

--- gestion_avion.py
+++ gestion_avion.py
@@ -1,16 +1,15 @@
 from avion import Avion
 from random import randint
 from utilities import distance_avion, import_json_avion, import_json_data
-from time import sleep
 
-FIR = "reims"
-L = []
+dict_data = {}
 dict_avion = {}
-dict_data = import_json_data(FIR)
-new_size_x = 621
-new_size_y = 431
 
-while True:
+def init_avion(FIR):
+    L = []
+    global dict_avion
+    global dict_data
+    dict_data = import_json_data(FIR)
     while len(dict_avion.keys()) <= 4:
         for i in range(5 + 1):
             if i not in dict_avion.keys():
@@ -36,7 +35,11 @@ while True:
                                                 int(dict_cara['vs']),
                                                 float(dict_cara['conso']),
                                                 int(dict_cara['alt']))
+    return dict_avion
 
+def gestion_avion(new_size_x = 621, new_size_y = 431):
+    global dict_avion
+    global dict_data
     scale_x = new_size_x / 621
     scale_y = new_size_y / 431
     for key in dict_avion.keys():
@@ -45,7 +48,7 @@ while True:
         dict_avion[key].vs_change()
         dict_avion[key].horizontal_move(scale_x, scale_y)
         dict_avion[key].vertical_move()
-        dict_avion[key].distance_aiport(dict_data[dict_cara['to']])
+        dict_avion[key].distance_aiport(dict_data[dict_avion[key]['to']])
         dict_avion[key].exit_scope(new_size_x, new_size_y)
         for key__ in dict_avion.keys():
             if key__ == key:
@@ -61,11 +64,11 @@ while True:
                     dict_avion.pop(key, None)
                     dict_avion.pop(key__, None)
         if dict_avion[key].consigne['landing'] == True:
-            dict_avion[key].landing(dict_data[dict_cara['to']])
+            dict_avion[key].landing(dict_data[dict_avion[key]['to']])
             dict_avion.pop(key, None)
 
     for key,value in dict_data.items():
         dict_data[key][0] = value[0] * scale_x
         dict_data[key][1] = value[1] * scale_y
 
-    sleep(1)
+    return dict_avion
